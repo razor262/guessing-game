d2b145ec57b0bba9fe12c0d2ab2b0a2d946a9866
razor262 <24.work@bk.ru>
2022-08-16 10:26:03 +0300

    this new commit

---
+++ src/guessing-game.js
@@ -0,0 +1,27 @@
+class GuessingGame {
+    constructor() {
+        this.num = Math.ceil(this.max / 2);
+        this.min;
+        this.max;
+    }
+
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    guess() {
+        this.num = Math.ceil((this.max - this.min) / 2 + this.min);
+        return this.num;
+    }
+
+    lower() {
+        this.setRange(this.min, this.num);
+    }
+
+    greater() {
+       this.setRange(this.num, this.max);
+    }
+}
+
+module.exports = GuessingGame;
